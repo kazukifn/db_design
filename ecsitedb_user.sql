3a3c0fce69aba6e6e034212c36a3e8dcece47a9a
kazukifn <oknsdnhrkm56@gmail.com>
2021-02-09 17:57:19 +0900

    fifth

--- ecsitedb_user.sql
+++ ecsitedb_user.sql
@@ -16,17 +16,17 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `usertable`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `usertable`;
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usertable` (
-  `userID` int NOT NULL,
+CREATE TABLE `user` (
+  `userID` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `ruby` varchar(45) NOT NULL,
-  `dt` date NOT NULL,
+  `furigana` varchar(45) NOT NULL,
+  `birthday` date NOT NULL,
   `gender` enum('男','女','その他') NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
@@ -34,18 +34,19 @@ CREATE TABLE `usertable` (
   `address1` text NOT NULL,
   `address2` text,
   `company` varchar(45) DEFAULT NULL,
-  `create_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
   PRIMARY KEY (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usertable`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `usertable` WRITE;
-/*!40000 ALTER TABLE `usertable` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usertable` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -57,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-04 17:36:22
+-- Dump completed on 2021-02-09 17:52:48
